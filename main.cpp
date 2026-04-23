069e6bf9a1c1cf080326bd785489573620b9653a
Kopano Molefi <kopanomolefi67@gmail.com>
2026-04-22 23:26:19 -0700

    Add PAT1 subtask2 code

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+    int first,second,third;
+    
+    count << "enter first temperature: ';
+    cin >> first;
+
+    cout << "enter second temperature:";
+    cin >> sencond;
+    
+    int difference = second - first;
+    
+    if (difference > 50) {
+       cout << "please lower the fryer heat"<< end1;
+    } else if (difference < 10) {
+      cout << "please raise the fryer heat" << end1;
+   
+
+   }
+
+   cout << "enter third tempereture:";
+   cin >> third;
+
+  if (third >=150 && third <= 190) {
+     cout << "oil is ready - start frying " << end1;
+  } else {
+     cout << "oil temperarure not suitable " << end1;
+  }
+  
+  return 0;
+}
+
+  
